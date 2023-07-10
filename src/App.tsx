1f60d35a6ea6f8a9c30fec2272a7f981e0a3ce5a
Rehan Hassan <rehan1579@gmail.com>
2023-07-11 04:45:44 +0500

    update MAIN & APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CssBaseline } from "@mui/material";
+import { QueryClientRoot, QueryClientRootInstance } from "./providers/query-client-provider";
+import { ThemeRoot, ThemeRootInstance } from "./providers/theme-provider";
+import { RouterProvider } from "react-router-dom";
+import ROUTES from "./root/routes";
+import "./App.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	return (
+		<ThemeRoot theme={ThemeRootInstance}>
+			<QueryClientRoot client={QueryClientRootInstance}>
+				<CssBaseline />
+				<RouterProvider router={ROUTES} />
+			</QueryClientRoot>
+		</ThemeRoot>
+	);
 }
-
-export default App
