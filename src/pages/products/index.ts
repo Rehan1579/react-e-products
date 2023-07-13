31c6cca6c54b7e3041eda6c0b2b641a5809489a1
Rehan Hassan <rehan1579@gmail.com>
2023-07-13 06:22:57 +0500

    create ProductAddEditPage

--- src/pages/products/index.ts
+++ src/pages/products/index.ts
@@ -3,6 +3,7 @@ import { lazy } from "react";
 
 const ProductListingPage = lazy(() => import("./product-listing/product-listing-page"));
 const ProductDetailPage = lazy(() => import("./product-detail/product-detail-page"));
+const ProductAddEditPage = lazy(() => import("./product-add-edit/product-add-edit-page"));
 
 
-export { ProductListingPage, ProductDetailPage };
+export { ProductListingPage, ProductDetailPage, ProductAddEditPage };
