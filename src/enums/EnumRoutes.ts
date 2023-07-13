f5673658a920440144815b0fce904a316948c0dd
Rehan Hassan <rehan1579@gmail.com>
2023-07-13 06:18:44 +0500

    Update EnumRoutes.ts

--- src/enums/EnumRoutes.ts
+++ src/enums/EnumRoutes.ts
@@ -2,4 +2,6 @@ export enum EnumRoutes {
 	HOME = "/",
 	PRODUCTS = "/products",
 	PRODUCT_DETAIL = "/products/:productId",
+	ADD_PRODUCT = "/products/add",
+	EDIT_PRODUCT = "/products/:productId/edit",
 }
