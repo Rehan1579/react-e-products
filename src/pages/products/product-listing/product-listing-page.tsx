121c5b0cefa15406bdea937ad7938a33141fa408
Rehan Hassan <rehan1579@gmail.com>
2023-07-13 06:22:27 +0500

    Update ProductListing

--- src/pages/products/product-listing/product-listing-page.tsx
+++ src/pages/products/product-listing/product-listing-page.tsx
@@ -1,28 +1,62 @@
 import styles from "./product-listing-page.module.scss"
 import { ProductService } from "@/services";
-import { useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { ProductPricing, ProductRating } from "@/components/product";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import { useState } from "react";
 import { EnumRoutes } from "@/enums";
-
+import EPagination from "@/components/shared/pagination";
+import { IPagination } from "@/interfaces/IPagination";
+import { EBusyLoader } from "@/components/shared";
+import TextField from "@mui/material/TextField";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from '@mui/icons-material/Add';
 
 
 export default function ProductListingPage()
 {   
+	const [params, _] = useSearchParams();
+	const page = Number(params.get("page")) || 1;
+	const category = params.get("category");
+
     const navigate = useNavigate();
     const [hoverId, setHoverId] = useState<string>("");
-    const { isLoading, isError, error, data: products } = useQuery(["product_listing"], () => ProductService.getAllProducts());
+    const [search, setSearch] = useState<string>("");
+
+    
+	const {
+		isLoading,
+		isError,
+		error,
+		data: productListing,
+	} = useQuery(["product_search", page], () => {
+		return category ? ProductService.getProductsByCategory(page, category) : ProductService.getProducts(page);
+	});
+
+
+	// const {
+	// 	isLoading,
+	// 	isError,
+	// 	error,
+	// 	data: searcResult,
+	// } = useQuery(["product_search", page], () => {
+	// 	return category ? ProductService.getProductsByCategory(page, category) : ProductService.getProducts(page);
+	// });
+
+
+
+	const handleSearchChange = (val: string) => {
+		console.log("searchProduct => " + val);
+		setSearch(val);
+	}
 
 
     if (isLoading)
     {
-		// return (
-		// 	// show skeleton
-		// );
+		return <EBusyLoader message="Loading Products" isLoading={isLoading} />;
 	}
 
 
@@ -39,44 +73,88 @@ export default function ProductListingPage()
     };
 
 
+	const changePage = (page: number) => {
+		const url = `${EnumRoutes.PRODUCTS}?page=${page}`;
+        navigate(url);
+	}
+
+
+	const getProductCount = (pagination: IPagination) => {
+		const totalCount = pagination.limit +  pagination.skip;
+		return (
+			<>Showing {totalCount} out of {pagination?.total}</>
+		)
+	}
+
+
     return (
 		<div className={styles.product_listing}>
 			<div className="container p-0">
-                <div className="row">
-                    <p className="col">Poducts {products?.length}</p>
-                </div>
-                <div className="row">
-                    {
-                        products?.map((item) => {
-                            return (
-								<div key={item.id} className="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 p-2">
-									<Card
-										sx={{ maxWidth: 345 }}
-										className={styles.product}
-										raised={hoverId == item.id}
-										onClick={() => showProductDetail(item.id)}
-										onMouseEnter={() => setHoverId(item.id)}
-										onMouseLeave={() => setHoverId("")}
-									>
-										<CardMedia sx={{ height: 140 }} image={item.thumbnail} title="Product Image" />
-										<CardContent className={styles.product_detail}>
-											<div className={styles.product_title}>
-												<p className={styles.product_title__text}>{item.title}</p>
-											</div>
+				<div className="row mb-4">
+					<div className="col">
+						<TextField
+							style={{ background: "#fff" }}
+							label="Search"
+							variant="outlined"
+							size="small"
+							fullWidth
+							value={search}
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+								handleSearchChange(event.target.value);
+							}}
+						/>
+					</div>
+					
+					<div className="col-auto">
+						<IconButton edge="start" color="inherit" aria-label="back" onClick={() => navigate(EnumRoutes.ADD_PRODUCT)}>
+							<AddIcon style={{color: "#1e1414"}} />
+						</IconButton>
+					</div>
+				</div>
 
-											<div className="mb-2">
-												<ProductPricing product={item} className={styles.product_pricing} />
-											</div>
+				<div className="row mb-2">
+					<div className="col">
+						<p className={`${styles.product__total_count} m-0`}>{getProductCount(productListing.pagination)}</p>
+					</div>
+				</div>
+				<div className="row mb-5">
+					{productListing.products?.map((item: any) => {
+						return (
+							<div key={item.id} className="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 p-2">
+								<Card
+									sx={{ maxWidth: 345 }}
+									className={styles.product}
+									raised={hoverId == item.id}
+									onClick={() => showProductDetail(item.id)}
+									onMouseEnter={() => setHoverId(item.id)}
+									onMouseLeave={() => setHoverId("")}
+								>
+									<CardMedia sx={{ height: 140 }} image={item.thumbnail} title="Product Image" />
+									<CardContent className={styles.product_detail}>
+										<div className={styles.product_title}>
+											<p className={styles.product_title__text}>{item.title}</p>
+										</div>
 
-											{item.rating > 0 && <ProductRating product={item} />}
-										</CardContent>
-									</Card>
-								</div>
-							);
-                        })
-                    }
-                </div>
-            </div>
+										<div className="mb-2">
+											<ProductPricing product={item} className={styles.product_pricing} />
+										</div>
+
+										{item.rating > 0 && <ProductRating product={item} />}
+									</CardContent>
+								</Card>
+							</div>
+						);
+					})}
+				</div>
+
+				{productListing.pagination && (
+					<div className="row justify-content-center">
+						<div className="col-auto">
+							<EPagination info={productListing.pagination} handlePaginationChange={changePage} />
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
